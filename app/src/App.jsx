2056ba679d1037a37fb5bf423e2f795900a24275
Jesse Huang <jessehuang@wustl.edu>
2017-07-17 15:39:58 +0800

    outline app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import NavBar from './components/NavBar';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
     </div>
   );
 }
