95645e62ea3cd5db01bd05046204e642c53ea84d
Jesse Huang <jessehuang@wustl.edu>
2017-08-05 16:08:19 -0700

    remove chatroom iframe

--- app/src/scenes/Projects/scenes/Chatroom.jsx
+++ app/src/scenes/Projects/scenes/Chatroom.jsx
@@ -19,9 +19,6 @@ const Chatroom = () => {
       <WorkInfo job={projects.Chatroom} style={{ whiteSpace: 'pre-wrap' }} />
       <a href="https://jessehuang.me:3456/chatroom.html">Website </a>
       <a href="https://github.com/jessejhuang/chatroom">GitHub</a>
-      <iframe style={frameStyle} title="project" src="http://jessehuang.me:3456/chatroom.html">
-        <p>Your browser does not support iframes.</p>
-      </iframe>
     </div>
   );
 };
