6eff8d7afd65844050717b384988cda2b0bab7f9
Jesse Huang <jessehuang@wustl.edu>
2017-08-05 16:14:41 -0700

    remove iframe

---
+++ app/src/scenes/Projects/scenes/Chatroom.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import WorkInfo from '../../../components/WorkInfo';
+import projects from '../constants/projects';
+
+const Chatroom = () => {
+  const style = {
+    padding: '1%',
+    fontSize: '18px',
+    textAlign: 'right',
+  };
+  return (
+    <div style={style}>
+      <WorkInfo job={projects.Chatroom} style={{ whiteSpace: 'pre-wrap' }} />
+      <a href="https://jessehuang.me:3456/chatroom.html">Website </a>
+      <a href="https://github.com/jessejhuang/chatroom">GitHub</a>
+    </div>
+  );
+};
+
+export default Chatroom;
