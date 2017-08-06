39076257311f5bff9e9d7d7125a5ef7456aa0ec1
Jesse Huang <jessehuang@wustl.edu>
2017-08-05 17:01:54 -0700

    reduce image sizes

--- app/src/scenes/Projects/scenes/Calendar.jsx
+++ app/src/scenes/Projects/scenes/Calendar.jsx
@@ -8,20 +8,10 @@ const Calendar = () => {
     fontSize: '18px',
     textAlign: 'right',
   };
-  const frameStyle = {
-    backgroundColor: 'white',
-    minHeight: '90vh',
-    width: '100%',
-    marginTop: '2%',
-  };
   return (
     <div style={style}>
       <WorkInfo job={projects.Calendar} style={{ whiteSpace: 'pre-wrap' }} />
       <a href="https://jessehuang.me/calendar">Website </a>
-      <a href="https://github.com/jessejhuang/calendar">GitHub</a>
-      <iframe style={frameStyle} title="project"src="https://jessehuang.me/calendar">
-        <p>Your browser does not support iframes.</p>
-      </iframe>
     </div>
   );
 };
